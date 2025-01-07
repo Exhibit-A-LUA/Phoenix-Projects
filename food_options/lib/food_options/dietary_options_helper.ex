5274e3be16c6641c1136e1af8603b10b5fb82c22
Andrew Berry <65485518+Exhibit-A-LUA@users.noreply.github.com>
2025-01-07 08:20:25 +0530

    updated selection to just use clickable text and other refinements

--- food_options/lib/food_options/dietary_options_helper.ex
+++ food_options/lib/food_options/dietary_options_helper.ex
@@ -1,26 +1,53 @@
 defmodule FoodOptions.DietaryOptionsHelper do
-  @options %{
-    "has_meat" => 1,
-    "has_red_meat" => 2,
-    "has_fish" => 4,
-    "has_seafood" => 8,
-    "has_poultry" => 16,
-    "has_dairy" => 32,
-    "has_egg" => 64,
-    "has_gluten" => 128,
-    "has_nuts" => 256,
-    "has_soy" => 512,
-    "has_legumes" => 1024,
-    "has_added_sugar" => 2048,
-    "has_alcohol" => 4096,
-    "is_processed" => 8192,
-    "is_spicy" => 16384,
-    "is_vegan" => 32768,
-    "is_halal" => 65536,
-    "is_kosher" => 131_072,
-    "is_low_fat" => 262_144,
-    "is_low_carb" => 524_288
-  }
+  # @options %{
+  #   "has_meat" => 1,
+  #   "has_red_meat" => 2,
+  #   "has_fish" => 4,
+  #   "has_seafood" => 8,
+  #   "has_poultry" => 16,
+  #   "has_dairy" => 32,
+  #   "has_egg" => 64,
+  #   "has_gluten" => 128,
+  #   "has_nuts" => 256,
+  #   "has_soy" => 512,
+  #   "has_legumes" => 1024,
+  #   "has_added_sugar" => 2048,
+  #   "has_alcohol" => 4096,
+  #   "is_processed" => 8192,
+  #   "is_spicy" => 16384,
+  #   "is_vegan" => 32768,
+  #   "is_halal" => 65536,
+  #   "is_kosher" => 131_072,
+  #   "is_low_fat" => 262_144,
+  #   "is_low_carb" => 524_288
+  # }
+
+  @options Enum.into(
+             Enum.with_index([
+               "has_meat",
+               "has_red_meat",
+               "has_fish",
+               "has_seafood",
+               "has_poultry",
+               "has_dairy",
+               "has_egg",
+               "has_gluten",
+               "has_nuts",
+               "has_soy",
+               "has_legumes",
+               "has_added_sugar",
+               "has_alcohol",
+               "is_processed",
+               "is_spicy",
+               "is_vegan",
+               "is_halal",
+               "is_kosher",
+               "is_low_fat",
+               "is_low_carb"
+             ]),
+             %{},
+             fn {option, index} -> {option, :math.pow(2, index) |> trunc()} end
+           )
 
   # Encode selected options into an integer bitstring
   def encode_options(selected_options) do
--- food_options/lib/food_options_web/live/dietary_option_live/index.ex
+++ food_options/lib/food_options_web/live/dietary_option_live/index.ex
@@ -13,14 +13,48 @@ defmodule FoodOptionsWeb.DietaryOptionLive.Index do
   end
 
   @impl true
-  def handle_event("convert_options", %{"options" => options}, socket) do
-    selected_options = Map.keys(options || %{})
+  def handle_event("toggle_option", %{"option" => option}, socket) do
+    selected_options =
+      if option in socket.assigns.selected_options do
+        socket.assigns.selected_options -- [option]
+      else
+        socket.assigns.selected_options ++ [option]
+      end
+
+    {:noreply, assign(socket, selected_options: selected_options)}
+  end
+
+  # @impl true
+  # def handle_event("convert_options", %{"options" => options} = _params, socket) do
+  #   selected_options = options || []
+  #   bitstring_value = DietaryOptionsHelper.encode_options(selected_options)
+
+  #   {:noreply,
+  #    assign(socket,
+  #      bitstring: bitstring_value,
+  #      selected_options: selected_options
+  #    )}
+  # end
+
+  @impl true
+  def handle_event("convert_options", %{"options" => options}, socket) when is_list(options) do
+    # If "options" exists and is a list
+    selected_options = options
     bitstring_value = DietaryOptionsHelper.encode_options(selected_options)
 
     {:noreply,
      assign(socket,
        bitstring: bitstring_value,
        selected_options: selected_options
+     )}
+  end
+
+  # Handle case where "options" key is missing entirely or is nil
+  def handle_event("convert_options", _params, socket) do
+    {:noreply,
+     assign(socket,
+       bitstring: 0,
+       selected_options: []
      )}
   end
 
